dd7c4c62c923edbf080b0d205957d22aad74c18e
Lucas ferry <lucas.ferry@actricity.com>
2024-06-03 13:18:27 +0200

    Rearrange main class and make code more clean.

--- testGame/Main.java
+++ testGame/Main.java
@@ -6,17 +6,34 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        DefaultView circleDefaultView = new DefaultView("Circle.png", Pivot.CENTER);
-        DefaultModel circleDefaultModel = new DefaultModel(
-                new Transform(
-                        new Position(400,300),
-                        new Scale(0.5)
+
+        Circle circle1 = new Circle(
+                new CircleView(Pivot.CENTER),
+                new CircleModel(
+                    new Transform(
+                            new Position(400,300),
+                            new Scale(0.5)
+                    )
                 )
         );
-        Circle circle = new Circle(circleDefaultView, circleDefaultModel);
 
-        Scene firstScene = new Scene(Color.RED);
-        firstScene.addGameObject(circle);
+        Circle circle2 = new Circle(
+                new CircleView(Pivot.CENTER),
+                new CircleModel(
+                        new Transform(
+                                new Position(200,300),
+                                new Scale(0.2)
+                        ),
+                        200
+                )
+        );
+
+        System.out.println(((CircleModel)circle1.model).getHp());
+        System.out.println(((CircleModel)circle2.model).getHp());
+
+        Scene firstScene = new Scene(Color.LIGHT_GRAY);
+        firstScene.addGameObject(circle1);
+        firstScene.addGameObject(circle2);
 
         Game TestGame = new Game("test");
         TestGame.addScene(firstScene);
