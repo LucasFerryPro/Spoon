4aae1e332391a950d6c055513d32da6e87b6108e
LucasFerryPro <lucas.ferry.ecole@gmail.com>
2024-06-10 11:18:44 +0200

    Revert "Change to Build Pattern"
    This reverts commit 2ad21faec45ce5c9ce81f536d85cfd3904cd8a57.

--- testGame/Main.java
+++ testGame/Main.java
@@ -6,34 +6,17 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        Circle circle1 = new Circle(
-                new CircleView(Pivot.CENTER),
-                new CircleModel(
-                    new Transform(
-                            new Position(400,300),
-                            new Scale(0.5)
-                    )
+        DefaultView circleDefaultView = new DefaultView("Circle.png", Pivot.CENTER);
+        DefaultModel circleDefaultModel = new DefaultModel(
+                new Transform(
+                        new Position(400,300),
+                        new Scale(0.5)
                 )
         );
+        Circle circle = new Circle(circleDefaultView, circleDefaultModel);
 
-        Circle circle2 = new Circle(
-                new CircleView(Pivot.CENTER),
-                new CircleModel(
-                        new Transform(
-                                new Position(200,300),
-                                new Scale(0.2)
-                        ),
-                        200
-                )
-        );
-
-        System.out.println(((CircleModel)circle1.model).getHp());
-        System.out.println(((CircleModel)circle2.model).getHp());
-
-        Scene firstScene = new Scene(Color.LIGHT_GRAY);
-        firstScene.addGameObject(circle1);
-        firstScene.addGameObject(circle2);
+        Scene firstScene = new Scene(Color.RED);
+        firstScene.addGameObject(circle);
 
         Game TestGame = new Game("test");
         TestGame.addScene(firstScene);
